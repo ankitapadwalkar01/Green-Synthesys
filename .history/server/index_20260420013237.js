822b56d1f62ae631f6629dd4946816ff8381db7d
REUEL CH <reuelch@REUELs-MacBook-Air.local>
2026-04-20 01:33:07 +0530

    fix gemini api call properly

--- .history/server/index_20260420013237.js
+++ .history/server/index_20260420013237.js
@@ -16,18 +16,8 @@ app.get("/", (req, res) => {
 
 app.post("/api/gemini", async (req, res) => {
   try {
-const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      contents: [{ parts: [{ text: prompt }] }],
-    }),
-  }
-);
+    c
+
     const data = await response.json();
 
   const text =
